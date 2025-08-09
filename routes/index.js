a86160866762f6c2060be72e49e496b98062c504
Milan Gonzalez <milangonzalez4@gmail.com>
2025-08-09 09:44:53 -0500

    Added about page

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => res.render('index'));
-router.get('/about', (req, res) => res.render('about'));
-router.get('/services', (req, res) => res.render('services'));
-router.get('/gallery', (req, res) => res.render('gallery'));
-router.get('/contact', (req, res) => res.render('contact'));
+router.get('/', (req, res) =>
+  res.render('index', { title: 'Home • YTX Media', page: 'home' })
+);
+
+router.get('/about', (req, res) =>
+  res.render('about', { title: 'About • YTX Media', page: 'about' })
+);
+
+router.get('/services', (req, res) =>
+  res.render('services', { title: 'Services • YTX Media', page: 'services' })
+);
+
+router.get('/gallery', (req, res) =>
+  res.render('gallery', { title: 'Gallery • YTX Media', page: 'gallery' })
+);
+
+router.get('/contact', (req, res) =>
+  res.render('contact', { title: 'Contact • YTX Media', page: 'contact' })
+);
 
 module.exports = router;
+
